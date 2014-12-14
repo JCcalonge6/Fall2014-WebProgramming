c0ec6006da733c5063cf1d0390568a98555df93b
JCcalonge6 <N02519919@hawkmail.newpaltz.edu>
2014-12-14 01:53:21 -0500

    Fitness Tracker
    Building a fitness tracker app

---
+++ FitnessTracker/food.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Fitness Tracker Food</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="view/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="view/css/style.min.css">
+	</head>
+	
+	<body>
+		<?php include_once("header.html"); ?>
+		<?php include_once("footer.html"); ?>
---
+++ FitnessTracker/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Fitness Tracker</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="view/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="view/css/style.min.css">
+	</head>
+	
+	<body>
+		<?php include_once("header.html"); ?>
+		<p>write stuff here</p>
+		<p>write stuff here</p>
+		<p>write stuff here</p>
+		<p>write stuff here</p>
+		<?php include_once("footer.html"); ?>
